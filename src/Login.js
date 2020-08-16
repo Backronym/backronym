5ed3842541b36a6a3394c26aad6b397781399fda
MhRantz <meganrantz@gmail.com>
2020-08-16 13:16:48 -0400

    styles again

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import backronyms from './assets/backronyms.jpg';
 
 class Login extends Component {
     render() {
         return (
             <div class="gridParent">
-                
+                <div className="login">
+                    <Link className="lightButton" to="/backronym">START</Link>
+                </div>
+                <div className="hero">
+
+                </div>
             </div>
         )
     }
