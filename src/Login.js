c7c9b21d193e3284a304343266a0d3ea83e750fe
MhRantz <meganrantz@gmail.com>
2020-08-16 12:12:04 -0400

    login

---
+++ src/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    render() {
+        return (
+            <div class="gridParent">
+                
+            </div>
+        )
+    }
+}
+
+export default Login;
