03842528cd9adda30b4e10931d73e59de9a3b4ac
MhRantz <meganrantz@gmail.com>
2020-08-17 15:06:10 -0400

    Updates

---
+++ precache-manifest.f6004ced5f32f60520a52f3b5b4ad2f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e448919685ac9293633dd1ee5644eb65",
+    "url": "/backronym/index.html"
+  },
+  {
+    "revision": "322e2bf51af84110e253",
+    "url": "/backronym/static/css/main.02417047.chunk.css"
+  },
+  {
+    "revision": "55d7b9f51143f7649a95",
+    "url": "/backronym/static/js/2.1c8e63df.chunk.js"
+  },
+  {
+    "revision": "ed2ca2bc1fcfad87ee81d05297bcf271",
+    "url": "/backronym/static/js/2.1c8e63df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "322e2bf51af84110e253",
+    "url": "/backronym/static/js/main.fa85205d.chunk.js"
+  },
+  {
+    "revision": "84aff303e48d5462833b",
+    "url": "/backronym/static/js/runtime-main.82208d48.js"
+  },
+  {
+    "revision": "7fa96c9a94620bcea59bf00806bd3269",
+    "url": "/backronym/static/media/backronyms.7fa96c9a.jpg"
+  }
+]);
