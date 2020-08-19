6f493bda95adfeb6b1f45653f9dda9f17f15d7d0
MhRantz <meganrantz@gmail.com>
2020-08-19 14:55:01 -0400

    small polishes

--- src/Login.js
+++ src/Login.js
@@ -1,29 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
+const Login = (props) => {
 
-class Login extends Component {
-    render() {
-        return (
-            <div className="gridParent">
-                <div className="login">
-                    <div className="controlsGap">
-                        <h1>BACKRONYM</h1>
-                        <button className="authButton secondarySButton" onClick={this.props.logIn}>Log In</button>
-                        <button className="authButton lightButton" onClick={this.props.guest}>Guest</button>
-                        <footer>
-                            <p>Copyright &copy; 2020:</p>
-                            <a href="https://meganrantz.io/">Megan</a>
-                            <a href="http://debyucodes.com/">Deb</a>
-                            <a href="http://twitter.com/alexorer">Ashwin</a>
-                            <a href="https://rahatrahman.com/">Rahat</a>
-                        </footer>
-                    </div>
+    return (
+        <div className="gridParent">
+            <div className="login">
+                <div className="controlsGap">
+                    <h1>BACKRONYM</h1>
+                    <button className="authButton secondarySButton" onClick={props.logIn}>Log In</button>
+                    <button className="authButton lightButton" onClick={props.guest}>Guest</button>
+                    <footer>
+                        <p>Copyright &copy; 2020:</p>
+                        <a href="https://meganrantz.io/">Megan</a>
+                        <a href="http://debyucodes.com/">Deb</a>
+                        <a href="http://twitter.com/alexorer">Ashwin</a>
+                        <a href="https://rahatrahman.com/">Rahat</a>
+                    </footer>
                 </div>
-                <div className="hero">
-                </div>
-
             </div>
-        )
-    }
+            <div className="hero">
+            </div>
+
+        </div>
+    )
 }
 
 export default Login;
